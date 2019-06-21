8333da3b682d71dd43e33e12318ff3095760c5f1
Jing Cao <jingcao.me@gmail.com>
2019-06-21 11:44:14 -0400

    added css to React mockup

--- src/App.js
+++ src/App.js
@@ -1,54 +1,67 @@
 import React, { Component } from "react";
 import "./App.css";
-import axios from "axios";
-import swim_dog from "./swim_dog.jpg"
-import art from "./picasso_style.jpg"
+import picasso_style from "./picasso_style.jpg";
+import picasso_cat from "./picasso_cat.png"
+import content_cat from "./persian_cat_content.jpg"
 
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      dogurl: "",
-      arturl: "https://apollo.imgix.net/content/uploads/2016/11/Picasso.jpg",
-      generatedurl: "",
-    };
-  }
-
-  async componentDidMount() {
-    //const art_data = await axios.get(this.state.arturl)
-    //console.log("art data is a hot mess of jpg noise")
-    try {
-      const { data: dog } = await axios.get(
-        "https://dog.ceo/api/breeds/image/random"
-      );
-      console.log("dog (is json w/ status and url)", dog)
-      this.setState({ dogurl: dog.message });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  render() {
+function App(props){
     return (
       <div className="App">
         <header className="App-header">
-        <h1>Welcome to my NST App!</h1>
+          <h1>Welcome to Picasso Cat!</h1>
+          <h2>This app uses neural style transfer (NST) to style your cat pics</h2>
         </header>
-        <div className="style-content-generated">
-          <p>Style Image:</p>
-          <img src={art} alt="art" />
-          <p>Content Image:</p>
-          <img src={swim_dog} alt="swimming dog" />
-          <p>Generated Image:</p>
-          <img src={this.state.dogurl} alt="a random dog" />
+        <div className="App-body">
+          <div className="Box">
+            <p>Style Image:</p>
+            <img src={picasso_style} alt="art" />
+          </div>
+          <div className="Box">
+            <p>Content Image:</p>
+            <img src={content_cat} alt="cat" />
+          </div>
+          <div className="Box">
+            <p>Generated Image:</p>
+            <img src={picasso_cat} alt="picasso cat" />
+          </div>
         </div>
       </div>
     );
   }
-}
 
 export default App;
+
+
+
+
+// for correctly formatting page w/ header and body
+
+
+//class App extends Component 
+
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     dogurl: "",
+  //     arturl: "https://apollo.imgix.net/content/uploads/2016/11/Picasso.jpg",
+  //     generatedurl: "",
+  //   };
+  // }
+
+//old code for grabbing random dog
+  // async componentDidMount() {
+  //   //const art_data = await axios.get(this.state.arturl)
+  //   //console.log("art data is a hot mess of jpg noise")
+  //   try {
+  //     const { data: dog } = await axios.get(
+  //       "https://dog.ceo/api/breeds/image/random"
+  //     );
+  //     console.log("dog (is json w/ status and url)", dog)
+  //     this.setState({ dogurl: dog.message });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
 
 //old code for grabbing and showing random art from artsy
 
